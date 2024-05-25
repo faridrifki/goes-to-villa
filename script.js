cdbb413d3f985c15430e2a766a7d4917a669a25a
Muhammad Farid <mfaridrifki@gmail.com>
2024-05-25 07:27:38 +0700

    revisi get time

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const tanggalTujuan = new Date('Jun 22, 2024 00:00:00:00').getTime();
+const tanggalTujuan = new Date("2024-06-22T00:00:00Z").getTime();
+const waktuHabis = document.querySelector('div.container');
 
-const realtimer = setInterval(function() {
-    const sekarang = new Date().getTime();
-    const selisih = tanggalTujuan - sekarang;
-    const hari = Math.floor(selisih / ( 1000 * 60 * 60 * 24 ));
-    const jam = Math.floor(selisih % ( 1000 * 60 * 60 * 24 ) / ( 1000 * 60 * 60 ));
-    const menit = Math.floor(selisih % ( 1000 * 60 * 60 ) / ( 1000 * 60));
-    const detik = Math.floor(selisih % ( 1000 * 60 ) / ( 1000 ));
-    
-    const h = document.getElementById('hari');
-    h.innerHTML = hari;
-    const j = document.getElementById('jam');
-    j.innerHTML = jam;
-    const m = document.getElementById('menit');
-    m.innerHTML = menit;
-    const d = document.getElementById('detik');
-    d.innerHTML = detik;
-}, 1000)
+const realtimer = setInterval(function () {
+  const sekarang = new Date().getTime();
+  const selisih = tanggalTujuan - sekarang;
+  const hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+  const jam = Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+  const detik = Math.floor((selisih % (1000 * 60)) / 1000);
 
+  const h = document.getElementById("hari");
+  h.innerHTML = hari;
+  const j = document.getElementById("jam");
+  j.innerHTML = jam;
+  const m = document.getElementById("menit");
+  m.innerHTML = menit;
+  const d = document.getElementById("detik");
+  d.innerHTML = detik;
+
+  if (selisih < 0) {
+    clearInterval(realtimer);
+    waktuHabis.innerHTML = 'time out'
+  }
+}, 1000);
